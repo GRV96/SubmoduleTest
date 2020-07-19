107cd08fb9384b7865273fb057651f0063abde9b
guyllaume.rousseau.1@ens.etstml.ca <guyllaume.rousseau.1@ens.etsmtl.ca>
2020-07-18 21:20:26 -0400

    Functions called in main.py from MockSubmodule

---
+++ main.py
@@ -0,0 +1,6 @@
+from os import system
+from MockSubmodule.astonishing_wonderful_functions import *
+
+print("Fantastic: ", super_ultimate_function(-2))
+print("Uninteresting: ", dull_function(1.2))
+system("pause")
