d3d966783073aab7ae74146c11b51529d56ac108
guyllaume.rousseau.1@ens.etstml.ca <guyllaume.rousseau.1@ens.etsmtl.ca>
2020-07-18 22:03:36 -0400

    Index value displayed in the console

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from MockSubmodule.astonishing_wonderful_functions import *
 index = LoopIndex(7)
 while index.iterate():
     i = index.get_value()
+    print("i =", i)
     print("Fantastic:", super_ultimate_function(i))
     print("Uninteresting:", dull_function(365.24 * i))
     print()
