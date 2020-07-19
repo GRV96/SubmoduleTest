4338b9e4e1796eb7699bd1fbab79120f734d2571
GRV96 <32419088+GRV96@users.noreply.github.com>
2020-07-18 22:06:08 -0400

    loopIndex into master (#1)
    * Main uses class LoopIndex.
    
    * Index value displayed in the console

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from os import system
+
+from LoopIndex.loop_index import LoopIndex
 from MockSubmodule.astonishing_wonderful_functions import *
 
-print("Fantastic: ", super_ultimate_function(-2))
-print("Uninteresting: ", dull_function(1.2))
+index = LoopIndex(7)
+while index.iterate():
+    i = index.get_value()
+    print("i =", i)
+    print("Fantastic:", super_ultimate_function(i))
+    print("Uninteresting:", dull_function(365.24 * i))
+    print()
+
 system("pause")
